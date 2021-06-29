50e92991cfd86f492a65482b4a5e0bdcd372d2e9
baizhen2 <baizhen2@illinois.edu>
2021-06-29 17:17:05 -0400

    Add task creation

--- engine.py
+++ engine.py
@@ -1,5 +1,7 @@
 import task
 import proxy
+import string
+import random
 
 class Engine:
 
@@ -9,6 +11,7 @@ class Engine:
 
         self.validProxies = []
         self.validAccounts = []
+        self.tasks = []
 
     def parseTextFile(self, file):
         f = open(file)
@@ -33,7 +36,16 @@ class Engine:
         for account in accounts:
             if account.find("@") != -1: #Checks for email
                 self.validAccounts.append(account)
-        
+    
+    def createTasks(self):
+        count = 0
 
+        for account in self.validAccounts:
+            if count >= len(self.validProxies): #reusing proxies if not enough
+                count = 0
+            
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+            newTask = task.Task(account, password, self.validProxies[count])
+            self.tasks.append(newTask)
 
-
+            count += 1
