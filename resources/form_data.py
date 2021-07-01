c8d03487c0624e78bad9887bc4f6214dd405c17b
baizhen2 <baizhen2@illinois.edu>
2021-07-01 11:20:18 -0400

    Add fanclub register form data

--- resources/form_data.py
+++ resources/form_data.py
@@ -1,9 +1,11 @@
 import json
+import random
 
-data = '{"firstName":"","lastName":"","email":"","password":"","newsletter":true,"accountOrigin":"FUNKO_COM"}'
+account_data = '{"firstName":"","lastName":"","email":"","password":"","newsletter":true,"accountOrigin":"FUNKO_COM"}'
+fanclub_data = '{"email":"","fanClubOptOut":false,"birthMonth":"","birthDay":"","captcharesponse":""}'
 
 def fillAccForm(firstName, lastName, email, password):
-    parsed = json.loads(data)
+    parsed = json.loads(account_data)
     parsed["firstName"] = firstName
     parsed["lastName"] = lastName
     parsed["email"] = email
@@ -12,10 +14,15 @@ def fillAccForm(firstName, lastName, email, password):
     json_string = json.dumps(parsed)
     return json_string
 
-
-
-
-
+def registerFanclub(email, captchaToken):
+    parsed = json.loads(fanclub_data)
     
+    parsed["email"] = email
+    parsed["captcharesponse"] = captchaToken
+    parsed["birthMonth"] = str(random.randint(2,9)) #Don't want to handle edge cases
+    parsed["birthDay"] = str(random.randint(2,25))
 
+    json_string = json.dumps(parsed)
+    return json_string
 
+print(registerFanclub("bob", "hello"))
