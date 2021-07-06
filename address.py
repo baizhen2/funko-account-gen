a537fcd31c2b9e272ce1d8ed55e148f694e0a15a
baizhen2 <baizhen2@illinois.edu>
2021-07-06 13:05:37 -0400

    Add address verification from funko-shopify servers

--- address.py
+++ address.py
@@ -1,13 +1,22 @@
 import requests
 import geojson
+import json
+import random
 from resources import form_data, headers
 
 class Address:
 
-    def __init__(self):
+    def __init__(self, session, firstName, lastName):
         self.data = form_data.address_data
         self.header = headers.address_verification_headers
         self.addresses = r'resources\random_address.geojson'
+
+        self.json_obj = None
+        self.address_data = None
+
+        self.session = session
+        self.firstName = firstName
+        self.lastName = lastName
     
     def formatGeoJson(self):
         with open(self.addresses) as file:
@@ -26,22 +35,39 @@ class Address:
                     file.write(line + ",\n")
                 line_count += 1
 
-    def parseJson(self):
         with open(self.addresses) as file:
             gj = geojson.load(file)
         
-        return gj
+        self.json_obj = gj
 
+    def formatAddress(self):
+        random_num = random.randint(0, len(self.json_obj) - 1)
 
+        address = self.json_obj[random_num]
+        city = address["properties"]["city"]
+        line_one = address["properties"]["number"] + " " + address["properties"]["street"]
+        state = address["properties"]["region"]
+        zipcode = address["properties"]["postcode"]
 
+        self.address_data = form_data.fillAddressInfo(self.firstName, self.lastName, line_one, city, state, zipcode)
 
+    def verifyAddress(self):
+        response = self.session.post('https://www.funko.com/api/address/verify', headers=self.header, data=self.address_data)
 
+        data = json.loads(response.text)
+                
+        try:
+            if data["exactMatch"] == False:
+                line_one = data["candidates"][0]["addressLine"]
+                city = data["candidates"][0]["city"]
+                state = data["candidates"][0]["state"]
+                zipcode = data["candidates"][0]["postalCode"]
+                self.address_data = form_data.fillAddressInfo(self.firstName, self.lastName, line_one, city, state, zipcode)
+            
+            if data["exactMatch"] == True:
+                return True
 
+        except KeyError: #Bad request to funko servers returning {'candidates': []}
+            return False
 
-""" response = requests.post('https://www.funko.com/api/address/verify', headers=headers, data=data)
-print(response)
-print(response.text) """
-
-new_address = Address()
-new_address.formatGeoJson()
-new_address.parseJson()
+        return False
