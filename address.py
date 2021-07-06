d4a04c48cf3e5455af1474c56eca7e9b383a1073
baizhen2 <baizhen2@illinois.edu>
2021-07-06 11:48:30 -0400

    Add OpenAddress geojson format convertor

---
+++ address.py
@@ -0,0 +1,43 @@
+import requests
+import geojson
+from resources import form_data, headers
+
+class Address:
+
+    def __init__(self):
+        self.data = form_data.address_data
+        self.header = headers.address_verification_headers
+        self.addresses = r'resources\random_address.geojson'
+    
+    def formatGeoJson(self):
+        with open(self.addresses) as file:
+            lines = file.read().splitlines()
+        
+        with open(self.addresses, "w") as file:
+            line_count = 0
+            last_line = len(lines) - 1
+
+            for line in lines:
+                if line_count == 0:
+                    file.write("[" + line + ",\n")
+                if line_count == last_line:
+                    file.write(line + "]")
+                else:
+                    file.write(line + ",\n")
+                line_count += 1
+
+
+
+
+
+
+
+
+
+
+""" response = requests.post('https://www.funko.com/api/address/verify', headers=headers, data=data)
+print(response)
+print(response.text) """
+
+new_address = Address()
+new_address.formatGeoJson()
